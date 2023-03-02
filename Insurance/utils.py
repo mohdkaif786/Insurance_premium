8fbeaee6f9cc420bc3260a5c528c90149a854681
mohdkaif786 <mohdkaif808@gmail.com>
2023-03-02 22:12:33 +0530

    utils file config filr updated

---
+++ Insurance/utils.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import numpy as np
+import os
+import sys
+from Insurance.exception import InsuranceExceotion
+from Insurance.config import mongo_client
+from Insurance.logger import logging
+
+def get_collection_as_dataframe(database_name:str, collection_name:str) -> pd.DataFrame:
+    try:
+        logging.info(f"Reading data from database:{database_name} and collection : {collection_name}")
+        df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"Find Columns: {df.columns}")
+        if "_id" in df.columns:
+            logging.info(f"Dropping columns: _id")
+            df = df.drop("_id", axis = 1)
+
+        logging.info(f"Rows and Columns in df: {df.shape}")
+        return df
+
+
+    except Exception as e:
+        raise InsuranceExceotion(e,sys)
--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from Insurance.logger import logging
 from Insurance.exception import InsuranceExceotion
 import os
 import sys
+from Insurance.utils import get_collection_as_dataframe
 
-def test_logger_and_exception():
-    try:
-        logging.info("Strating thr test_logger_and_exception")
-        result = 3/0
-        print(result)
-        logging.info("Ending point of the test_logger_and_exception")
-    except Exception as e:
-        logging.debug(str(e))
-        raise InsuranceExceotion(e,sys)
+#def test_logger_and_exception():
+   # try:
+   #     logging.info("Strating thr test_logger_and_exception")
+   #     result = 3/0
+   #     print(result)
+   #     logging.info("Ending point of the test_logger_and_exception")
+   # except Exception as e:
+   #     logging.debug(str(e))
+   #     raise InsuranceExceotion(e,sys)
     
 
 if __name__ == "__main__":
     try:
-        test_logger_and_exception()
+        #test_logger_and_exception()
+        get_collection_as_dataframe(database_name = "INSURANCE", collection_name = "INSURANCE_PROJECT")
     except Exception as e:
         print(e)
 
